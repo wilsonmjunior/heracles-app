b3b55f43e5f62f748f9996f8c53a252cfd0f4544
Wilson Junior <wilson.junior@merx.tech>
2023-05-28 20:05:09 -0300

    feat: add assets

---
+++ src/assets/index.ts
@@ -0,0 +1,22 @@
+import backgroundImg from './images/background.png'
+import userPhotoDefaultImg from './images/userPhotoDefault.png'
+
+import bodySvg from './svgs/body.svg';
+import historySvg from './svgs/history.svg';
+import homeSvg from './svgs/home.svg';
+import logoSvg from './svgs/logo.svg';
+import profileSvg from './svgs/profile.svg';
+import repetitionsSvg from './svgs/repetitions.svg';
+import seriesSvg from './svgs/series.svg';
+
+export {
+  backgroundImg,
+  userPhotoDefaultImg,
+  bodySvg,
+  historySvg,
+  homeSvg,
+  logoSvg,
+  profileSvg,
+  repetitionsSvg,
+  seriesSvg,
+}
