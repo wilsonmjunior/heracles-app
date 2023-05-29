79ff5a79919d91eb8e54967131a9ef14d5f3dbe4
Wilson Junior <wilson.junior@merx.tech>
2023-05-28 23:28:18 -0300

    feat: add variant outline

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,27 +6,34 @@ import {
 
 type ButtonProps = IButtonProps & {
   title: string;
+  variant?: "outline" | "solid";
 }
 
-export function Button({ title, ...props }: ButtonProps) {
+export function Button({
+  title,
+  variant = "solid",
+  ...othersProps
+}: ButtonProps) {
   return (
     <NBButton
       w="full"
       h={14}
-      bg="green.700"
+      bg={variant === "outline" ? "transparent" : "green.700"}
+      borderWidth={variant === "outline" ? 1 : 0}
+      borderColor="green.700"
       rounded="sm"
       _pressed={{
-        bg: "green.500"
+        bg: variant === "outline" ? "gray.500" : "green.500"
       }}
-      {...props}
+      {...othersProps}
     >
       <Text
-        color="white"
+        color={variant === "outline" ? "green.500" : "white"}
         fontFamily="heading"
         fontSize="sm"
       >
         {title}
       </Text>
-    </NBButton>
+    </NBButton >
   )
 }
