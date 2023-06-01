7954be50de015329f7e834ae9bcb531f275942f9
Wilson Junior <wilson.junior@merx.tech>
2023-05-31 23:40:52 -0300

    feat: create routes

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,28 @@
+import { BottomTabNavigationProp, createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+
+import { Home } from "@screens/Home"
+import { History } from "@screens/History"
+import { Profile } from "@screens/Profile"
+import { Exercise } from "@screens/Exercise"
+
+type AppRoutesType = {
+  home: undefined;
+  history: undefined;
+  profile: undefined;
+  exercise: undefined;
+}
+
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutesType>
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutesType>()
+
+export function AppRoutes() {
+  return (
+    <Navigator>
+      <Screen name="home" component={Home} />
+      <Screen name="history" component={History} />
+      <Screen name="profile" component={Profile} />
+      <Screen name="exercise" component={Exercise} />
+    </Navigator>
+  )
+}
