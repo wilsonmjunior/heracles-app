1d948b0053759a61e54f06d038e244e2acc3e10e
Wilson M Junior <wilsonmjuniorx@gmail.com>
2023-06-04 00:58:40 -0300

    feat: add error

--- src/hooks/reducer.hook.ts
+++ src/hooks/reducer.hook.ts
@@ -1,20 +1,44 @@
 import { useReducer } from "react"
 
+export type FieldState = {
+  value: string;
+  error: string;
+}
+
+type State = {
+  [key: string]: FieldState;
+}
+
 type Action = {
   type: string;
   field: string;
   value: string;
+  error?: string;
 }
 
-export function useReducerController<T>(initialState: T) {
+export function useReducerController<T extends State>(initialState: T) {
   function reducer(state: T, action: Action): T {
     switch (action.type) {
       case 'UPDATE_FIELD':
-        return { ...state, [action.field]: action.value }
+        return { 
+          ...state, 
+          [action.field]: { 
+            value: action.value, 
+            error: '', 
+          } 
+        };
+      case 'SET_ERROR':
+        return { 
+          ...state, 
+          [action.field]: { 
+            ...state[action.field], 
+            error: action.error, 
+          }
+        };
       default:
-        return state
+        return state;
     }
-  }  
+  }
 
-  return useReducer(reducer, initialState)
+  return useReducer(reducer, initialState);
 }
