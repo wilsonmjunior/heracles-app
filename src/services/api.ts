991367129256b8dd2927553e90d0b4467f58b1f8
Wilson M Junior <wilsonmjuniorx@gmail.com>
2023-06-06 14:15:53 -0300

    feat: create app error class

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+import { AppError } from "@utils/AppError";
+
 const api = axios.create({
   baseURL: "http://192.168.0.108:3333",
 })
@@ -7,13 +9,18 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
   return config;
 }, (error) => {
+
   return Promise.reject(error);
 })
 
 api.interceptors.response.use((response) => {
   return response;
 }, (error) => {
-  return Promise.reject(error);
+  if (axios.isAxiosError(error) && error.response?.data) {
+    return Promise.reject(new AppError(error.response.data.message));
+  }
+
+  return Promise.reject(new AppError("Error no servidor. Tente novamente mais tarde"));
 })
 
 export { api }
