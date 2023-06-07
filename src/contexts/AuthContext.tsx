f484b11e4dc7319ac85b88f820d35508251fdcba
Wilson M Junior <wilsonmjuniorx@gmail.com>
2023-06-06 23:00:42 -0300

    feat: add show success message

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,9 +1,11 @@
-import { ReactNode, createContext, useState } from "react"
+import { ReactNode, createContext, useCallback, useState } from "react"
 
 import { UserDTO } from "@dtos/UserDTO";
+import { api } from "@services/api";
 
 type AuthContextDataProps = {
   user: UserDTO;
+  signIn(email: string, password: string): Promise<void>
 }
 
 type AuthProviderProps = { 
@@ -13,13 +15,20 @@ type AuthProviderProps = {
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps)
 
 export function AuthProvider({ children  }:AuthProviderProps) {
-  const [user, setUser] = useState({
-    name: "Wilow",
-  } as UserDTO);
+  const [user, setUser] = useState<UserDTO>({} as UserDTO);
+
+  const signIn = useCallback(async (email: string, password: string) => {
+    try {
+      const response = await api.post('sessions', { email, password });
+      setUser(response.data)
+    } catch (error) {
+      throw error;
+    }
+  }, [])
   
   return (
     <AuthContext.Provider
-      value={{ user }}
+      value={{ user, signIn }}
     >
       {children}
     </AuthContext.Provider>
